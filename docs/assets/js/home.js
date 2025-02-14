54f1ddb3d950e2318b8897c38297421e662ef390
eduardoparalegal <eduardoparalegalsv@gmail.com>
2025-02-14 08:54:48 -0600

    Tu mensaje de commit

--- docs/assets/js/home.js
+++ docs/assets/js/home.js
@@ -1,47 +1,22 @@
-// WIKI-PARALEGAL-STATIC/docs/assets/js/home.js
-$(document).ready(function() {
-    // Configuración del nombre del usuario
-    const userName = "Usuario"; // Esto podría venir de una variable de sesión
-    $("#user-name").text(userName);
-
-    // Inicialización del selector activo
-    var tabsVerticalInner = $('#accordian');
-    var activeItemVerticalInner = tabsVerticalInner.find('.active');
-    var activeWidthVerticalHeight = activeItemVerticalInner.innerHeight();
-    var activeWidthVerticalWidth = activeItemVerticalInner.innerWidth();
-    var itemPosVerticalTop = activeItemVerticalInner.position();
-    var itemPosVerticalLeft = activeItemVerticalInner.position();
-
-    $(".selector-active").css({
-        "top": itemPosVerticalTop.top + "px",
-        "left": itemPosVerticalLeft.left + "px",
-        "height": activeWidthVerticalHeight + "px",
-        "width": activeWidthVerticalWidth + "px"
-    });
-
-    // Manejador de clics para los elementos del menú
-    $("#accordian").on("click", "li", function(e) {
-        $('#accordian ul li').removeClass("active");
-        $(this).addClass('active');
-
-        var activeWidthVerticalHeight = $(this).innerHeight();
-        var activeWidthVerticalWidth = $(this).innerWidth();
-        var itemPosVerticalTop = $(this).position();
-        var itemPosVerticalLeft = $(this).position();
-
-        $(".selector-active").css({
-            "top": itemPosVerticalTop.top + "px",
-            "left": itemPosVerticalLeft.left + "px",
-            "height": activeWidthVerticalHeight + "px",
-            "width": activeWidthVerticalWidth + "px"
-        });
-    });
-
-    // Agregar clase active basada en la página actual
-    var path = window.location.pathname.split("/").pop();
-    if (path == '') {
-        path = 'home.html';
+$(document).ready(function(){
+	$('#nav-icon').click(function(){
+		$(this).toggleClass('open');
+    $(".overlay").toggleClass("open");
+    $(".overlay a").toggleClass("open");
+  $(".overlay p").toggleClass("open");
+	});
+  
+  // Scroll Reveal
+  // ScrollReveal().reveal('.services .grid .card');
+  
+  $('.square.blue').on('inview', function(event, isInView) {
+    if (isInView) {
+      // element is now visible in the viewport
+      $(this).addClass("in-view");
+    } else {
+      // element has gone out of viewport
+      $(this).removeClass("in-view");
     }
-    var target = $('#accordian ul li a[href="' + path + '"]');
-    target.parent().addClass('active');
+  });
 });
+ 
